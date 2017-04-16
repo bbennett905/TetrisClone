d866a399f233a02c0b3708e779fcc38ccaa4fc3a
Brett Bennett <bbennett905@gmail.com>
2017-04-16 15:29:39 -0400

    Add a number of playfield functions and implementations

--- src/playfield.c
+++ src/playfield.c
@@ -5,9 +5,11 @@
 //only really need to save the color of each block, not its position
 block_color field[FIELD_SIZE];
 tetromino_t* active_tetromino;
+tetromino_shape next;
 
 void init_field()
 {
+	next = rand() % NUM_TETROMINO_SHAPES;
 	for (int i = 0; i < FIELD_SIZE; i++)
 	{
 		field[i] = COLOR_NONE;
@@ -22,4 +24,92 @@ bool is_block_at(vec2d8_t pos)
 block_color color_at(vec2d8_t pos)
 {
 	return field[(pos.x * pos.y) - 1];
+}
+
+tetromino_shape get_next()
+{
+	return next;
+}
+
+void kill_active_tetromino()
+{
+	for (int i = 0; i < 4; i++)
+	{
+		vec2d8_t temp_pos = add(active_tetromino->_pos, active_tetromino->_blocks[i]._pos);
+		uint8_t field_pos = temp_pos.y * FIELD_WIDTH + temp_pos.x;
+		field[field_pos] = active_tetromino->_blocks[i]._color;
+	}
+	free(active_tetromino);
+	active_tetromino = NULL;
+}
+
+void spawn_tetromino()
+{
+	//make sure to srand((unsigned) time(time_t* t)) in main or something
+	//This is random enough for this purpose
+	tetromino_shape shape = next;
+	next = rand() % NUM_TETROMINO_SHAPES;
+	tetromino_t* t = (tetromino_t*)malloc(sizeof(tetromino_t));
+	
+	for (int i = 0; i < 4; i++)
+	{
+		t->_blocks[i]._color = shape + 1;
+	}
+
+	//Default spawn position
+	t->_pos.x = 3;
+	t->_pos.y = 21;
+
+	switch (shape)
+	{
+	case TETROMINO_I:
+		//Altered spawn position
+		t->_pos.y = 20;
+		
+		t->_blocks[0]._pos = (vec2d8_t) { 0, 0 };
+		t->_blocks[0]._pos = (vec2d8_t) { 1, 0 };
+		t->_blocks[0]._pos = (vec2d8_t) { 2, 0 };
+		t->_blocks[0]._pos = (vec2d8_t) { 3, 0 };
+		break;
+	case TETROMINO_L:
+		t->_blocks[0]._pos = (vec2d8_t) { 0, -1 };
+		t->_blocks[0]._pos = (vec2d8_t) { 1, -1 };
+		t->_blocks[0]._pos = (vec2d8_t) { 2, -1 };
+		t->_blocks[0]._pos = (vec2d8_t) { 2, 0 };
+		break;
+	case TETROMINO_REVERSE_L:
+		t->_blocks[0]._pos = (vec2d8_t) { 0, -1 };
+		t->_blocks[0]._pos = (vec2d8_t) { 1, -1 };
+		t->_blocks[0]._pos = (vec2d8_t) { 2, -1 };
+		t->_blocks[0]._pos = (vec2d8_t) { 0, 0 };
+		break;
+	case TETROMINO_SQUARE:
+		//Altered spawn position
+		t->_pos.x = 4;
+
+		t->_blocks[0]._pos = (vec2d8_t) { 0, 0 };
+		t->_blocks[0]._pos = (vec2d8_t) { 1, 0 };
+		t->_blocks[0]._pos = (vec2d8_t) { 0, -1 };
+		t->_blocks[0]._pos = (vec2d8_t) { 1, -1 };
+		break;
+	case TETROMINO_S:
+		t->_blocks[0]._pos = (vec2d8_t) { 0, -1 };
+		t->_blocks[0]._pos = (vec2d8_t) { 1, -1 };
+		t->_blocks[0]._pos = (vec2d8_t) { 1, 0 };
+		t->_blocks[0]._pos = (vec2d8_t) { 2, 0 };
+		break;
+	case TETROMINO_REVERSE_S:
+		t->_blocks[0]._pos = (vec2d8_t) { 0, 0 };
+		t->_blocks[0]._pos = (vec2d8_t) { 1, -1 };
+		t->_blocks[0]._pos = (vec2d8_t) { 1, 0 };
+		t->_blocks[0]._pos = (vec2d8_t) { 2, -1 };
+		break;
+	case TETROMINO_T:
+		t->_blocks[0]._pos = (vec2d8_t) { 0, -1 };
+		t->_blocks[0]._pos = (vec2d8_t) { 1, -1 };
+		t->_blocks[0]._pos = (vec2d8_t) { 1, 0 };
+		t->_blocks[0]._pos = (vec2d8_t) { 2, -1 };
+		break;
+	}
+	active_tetromino = t;
 }
--- src/playfield.h
+++ src/playfield.h
@@ -8,8 +8,15 @@
 #define FIELD_WIDTH 10
 #define FIELD_SIZE (FIELD_HEIGHT * FIELD_WIDTH)
 
+//Initialize the playfield
 void init_field();
+//Returns true if there is a block at this field position
 bool is_block_at(vec2d8_t pos);
+//Returns the color of a block at this position
 block_color color_at(vec2d8_t pos);
+//Returns the shape of the next tetromino
+tetromino_shape get_next();
+//Kills the currently active tetromino and updates the field blocks; frees the tetromino
 void kill_active_tetromino();
-void
+//Spawns a random new tetromino and sets it as active
+void spawn_tetromino();
